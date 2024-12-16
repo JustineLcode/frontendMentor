68ef3f9b0cf29fa5f4b28d9d54f433c8f2777465
JustineLcode <leulierjustine@hotmail.com>
2024-12-16 16:10:15 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [advice, setAdvice] = useState([]);
+
+  const fetchData = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    console.log(data);
+    console.log(data.slip.id);
+    setAdvice(data.slip);
+  };
+  useEffect (()=> { 
+    fetchData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="card">
+      <h5>ADVICE #{`${advice.id}`}</h5>
+      <p>{`"${advice.advice}"`}</p>
+      <div className='pause'>
+        <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg">
+        <g fill="none" fill-rule="evenodd">
+          <path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/>
+        <g transform="translate(212)" fill="#CEE3E9">
+          <rect width="6" height="16" rx="3"/>
+          <rect x="14" width="6" height="16" rx="3"/>
+          </g></g></svg>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <a className='dé' href="/">
+        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/>
+        </svg>
+      </a>
+    </div>
     </>
   )
 }
